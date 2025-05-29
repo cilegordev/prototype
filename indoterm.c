53392e750ebeba62f80d8eb46e5a1dfd638c12b5
cilegordev <deputyalectro@icloud.com>
2025-05-29 11:29:38 +0700

    newterm

--- indoterm.c
+++ indoterm.c
@@ -1,6 +1,5 @@
 // dep : gtk3 vte
 // Penulis : Cilegordev & Dibuat bareng ChatGPT 🤖✨
-// import version: 0.5.2-beta
 
 #include <gtk/gtk.h>
 #include <vte/vte.h>
@@ -27,15 +26,12 @@ static void on_switch_page(GtkNotebook *notebook, GtkWidget *page, guint page_nu
 
 static void on_selection_changed(VteTerminal *terminal, gpointer user_data) {
     vte_terminal_copy_clipboard_format(terminal, VTE_FORMAT_TEXT);
-
     vte_terminal_set_scroll_on_output(terminal, FALSE);
 }
 
 static void update_tab_visibility() {
     int page_count = gtk_notebook_get_n_pages(GTK_NOTEBOOK(notebook));
-
     gtk_notebook_set_show_tabs(GTK_NOTEBOOK(notebook), page_count > 1);
-
     if (page_count == 0) {
         gtk_window_close(GTK_WINDOW(main_window));
     }
@@ -94,7 +90,6 @@ static void on_cwd_changed(VteTerminal *terminal, GParamSpec *pspec, gpointer us
     data->timeout_id = g_timeout_add(100, check_cwd_change, data);
 }
 
-//BADPIG
 static void copy_terminal_text(GtkWidget *widget, gpointer user_data) {
     VteTerminal *terminal = VTE_TERMINAL(user_data);
     vte_terminal_copy_clipboard_format(terminal, VTE_FORMAT_TEXT);
@@ -103,13 +98,11 @@ static void copy_terminal_text(GtkWidget *widget, gpointer user_data) {
 static gboolean scroll_terminal_bottom(gpointer user_data) {
     VteTerminal *terminal = VTE_TERMINAL(user_data);
     GtkAdjustment *adjustment = gtk_scrollable_get_vadjustment(GTK_SCROLLABLE(terminal));
-
     if (adjustment) {
         gdouble upper = gtk_adjustment_get_upper(adjustment);
         gdouble page_size = gtk_adjustment_get_page_size(adjustment);
         gtk_adjustment_set_value(adjustment, upper - page_size);
     }
-
     return G_SOURCE_REMOVE;
 }
 
@@ -122,19 +115,14 @@ static void paste_terminal_text(GtkWidget *widget, gpointer user_data) {
 static gboolean on_button_press(GtkWidget *widget, GdkEventButton *event, gpointer user_data) {
     if (event->type == GDK_BUTTON_PRESS && event->button == 3) {
         GtkWidget *menu = gtk_menu_new();
-
         GtkWidget *copy_item = gtk_menu_item_new_with_label("Copy");
         GtkWidget *paste_item = gtk_menu_item_new_with_label("Paste");
-
         g_signal_connect(copy_item, "activate", G_CALLBACK(copy_terminal_text), user_data);
         g_signal_connect(paste_item, "activate", G_CALLBACK(paste_terminal_text), user_data);
-
         gtk_menu_shell_append(GTK_MENU_SHELL(menu), copy_item);
         gtk_menu_shell_append(GTK_MENU_SHELL(menu), paste_item);
-
         gtk_widget_show_all(menu);
         gtk_menu_popup_at_pointer(GTK_MENU(menu), (GdkEvent*)event);
-
         return TRUE;
     }
     return FALSE;
@@ -151,22 +139,16 @@ static GtkWidget* create_tab_label(GtkWidget *child, TerminalData *data) {
     GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 4);
     GtkWidget *label = gtk_label_new("Terminal");
     GtkWidget *close_btn = gtk_button_new_with_label("⨯");
-
     g_signal_connect_swapped(close_btn, "clicked", G_CALLBACK(gtk_widget_destroy), child);
     g_signal_connect_swapped(close_btn, "clicked", G_CALLBACK(update_tab_visibility), NULL);
-
     data->label = label;
-
     gtk_button_set_relief(GTK_BUTTON(close_btn), GTK_RELIEF_NONE);
     gtk_widget_set_can_focus(close_btn, FALSE);
     gtk_widget_set_valign(close_btn, GTK_ALIGN_CENTER);
-
     gtk_widget_set_hexpand(label, TRUE);
     gtk_label_set_xalign(GTK_LABEL(label), 0.0);
-
     gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(box), close_btn, FALSE, FALSE, 0);
-
     gtk_widget_show_all(box);
     return box;
 }
@@ -176,25 +158,20 @@ static void on_terminal_exit(VteTerminal *terminal, gint status, gpointer user_d
     if (data->timeout_id) {
         g_source_remove(data->timeout_id);
     }
-
     GtkWidget *page = gtk_widget_get_parent(GTK_WIDGET(terminal));
     gtk_widget_destroy(page);
-
     update_tab_visibility();
-
     g_free(data->last_cwd);
     g_free(data);
 }
 
 static void on_terminal_spawn_success(VteTerminal *terminal, GPid pid, GError *error, gpointer user_data) {
     TerminalData *data = (TerminalData *)user_data;
-
     if (error != NULL) {
         g_warning("Failed to spawn terminal: %s", error->message);
         g_error_free(error);
         return;
     }
-
     data->pid = pid;
     data->timeout_id = g_timeout_add(100, check_cwd_change, data);
     update_tab_title(data);
@@ -207,42 +184,24 @@ static GtkWidget* create_terminal_tab(TerminalData **data_ptr) {
     vte_terminal_set_scrollback_lines(VTE_TERMINAL(terminal), -1);
     gtk_container_add(GTK_CONTAINER(scrolled_window), terminal);
     vte_terminal_set_scroll_on_output(VTE_TERMINAL(terminal), FALSE);
-    
+
     TerminalData *data = g_new0(TerminalData, 1);
     data->terminal = terminal;
     data->pid = -1;
     data->last_cwd = NULL;
     data->timeout_id = 0;
     *data_ptr = data;
-
     g_object_set_data(G_OBJECT(scrolled_window), "terminal-data", data);
-    
-    //BADPIG
-    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
 
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
     g_signal_connect(terminal, "button-press-event", G_CALLBACK(on_button_press), terminal);
     g_signal_connect(terminal, "notify::current-directory-uri", G_CALLBACK(on_cwd_changed), data);
 
     char *cwd = g_get_current_dir();
     const char *user_shell = g_getenv("SHELL");
-    if (!user_shell) {
-        user_shell = "/bin/sh";
-    }
+    if (!user_shell) user_shell = "/bin/sh";
     char *argv[] = { (char *)user_shell, NULL };
-    vte_terminal_spawn_async(
-        VTE_TERMINAL(terminal),
-        VTE_PTY_DEFAULT,
-        cwd,
-        argv,
-        NULL,
-        G_SPAWN_DEFAULT,
-        NULL, NULL,
-        NULL, -1,
-        NULL,
-        on_terminal_spawn_success,
-        data
-    );
-
+    vte_terminal_spawn_async(VTE_TERMINAL(terminal), VTE_PTY_DEFAULT, cwd, argv, NULL, G_SPAWN_DEFAULT, NULL, NULL, NULL, -1, NULL, on_terminal_spawn_success, data);
     g_signal_connect(terminal, "child-exited", G_CALLBACK(on_terminal_exit), data);
     g_free(cwd);
 
@@ -254,26 +213,76 @@ static void on_new_tab(GtkButton *button, gpointer user_data) {
     TerminalData *data = NULL;
     GtkWidget *terminal = create_terminal_tab(&data);
     GtkWidget *tab_label = create_tab_label(terminal, data);
-
     int page_num = gtk_notebook_append_page(nb, terminal, tab_label);
-
     gtk_widget_show_all(terminal);
     gtk_notebook_set_current_page(nb, page_num);
     gtk_widget_grab_focus(terminal);
-
     update_tab_visibility();
+}
+
+static void tentang_terminal(GtkButton *button, gpointer user_data) {
+    GtkWidget *dialog = gtk_dialog_new_with_buttons(
+        "_Tutup",
+        GTK_WINDOW(main_window),
+        GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+        "_Tutup", GTK_RESPONSE_CLOSE,
+        NULL
+    );
+
+    gtk_window_set_title(GTK_WINDOW(dialog), "Tentang Terminal Ini");
+    gtk_window_set_default_size(GTK_WINDOW(dialog), 360, 144);
+
+    GtkWidget *header = gtk_header_bar_new();
+    gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header), TRUE);
+    gtk_header_bar_set_title(GTK_HEADER_BAR(header), "indoterm v1.0.0");
+    gtk_window_set_titlebar(GTK_WINDOW(dialog), header);
+
+    GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_set_border_width(GTK_CONTAINER(vbox), 10);
+
+    GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    GtkWidget *icon = gtk_image_new_from_icon_name("utilities-terminal", GTK_ICON_SIZE_DIALOG);
+    gtk_widget_set_halign(icon, GTK_ALIGN_START);
+    gtk_widget_set_valign(icon, GTK_ALIGN_START);
+
+    GtkWidget *label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label),
+        "Di compile dengan : \n"
+        "GCC v14.2.0\n"
+        "GTK+-3.0 v3.24.49\n"
+        "VTE-2.91 v0.80.1\n"
+        "<a href=\"https://github.com/cilegordev/prototype/blob/main/indoterm.c\">Lihat Halaman Projek Ini</a>");
+    gtk_label_set_xalign(GTK_LABEL(label), 0.0);
+
+    gtk_box_pack_start(GTK_BOX(hbox), icon, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), label, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), hbox, TRUE, TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(content_area), vbox);
+
+    gtk_widget_show_all(dialog);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+
+    gint current_page = gtk_notebook_get_current_page(GTK_NOTEBOOK(notebook));
+    GtkWidget *page = gtk_notebook_get_nth_page(GTK_NOTEBOOK(notebook), current_page);
+    if (page) {
+        TerminalData *data = g_object_get_data(G_OBJECT(page), "terminal-data");
+        if (data && data->terminal) {
+            gtk_widget_grab_focus(GTK_WIDGET(data->terminal));
+        }
+    }
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
     main_window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(main_window), "indoterm-dev");
+    gtk_window_set_title(GTK_WINDOW(main_window), "Indonesia Terminal");
     gtk_window_set_default_size(GTK_WINDOW(main_window), 824, 472);
     gtk_window_set_icon_name(GTK_WINDOW(main_window), "utilities-terminal");
-
     gtk_window_set_position(GTK_WINDOW(main_window), GTK_WIN_POS_CENTER);
 
     header = gtk_header_bar_new();
-    gtk_header_bar_set_title(GTK_HEADER_BAR(header), "indoterm-dev");
+    gtk_header_bar_set_title(GTK_HEADER_BAR(header), "indoterm");
     gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header), TRUE);
     gtk_window_set_titlebar(GTK_WINDOW(main_window), header);
 
@@ -281,22 +290,28 @@ static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *new_tab_btn = gtk_button_new();
     gtk_button_set_relief(GTK_BUTTON(new_tab_btn), GTK_RELIEF_NONE);
     gtk_container_add(GTK_CONTAINER(new_tab_btn), new_tab_icon);
+
+    GtkWidget *menu_icon = gtk_image_new_from_icon_name("gtk-about", GTK_ICON_SIZE_BUTTON);
+    GtkWidget *menu_btn = gtk_button_new();
+    gtk_button_set_relief(GTK_BUTTON(menu_btn), GTK_RELIEF_NONE);
+    gtk_container_add(GTK_CONTAINER(menu_btn), menu_icon);
+
+    gtk_header_bar_pack_end(GTK_HEADER_BAR(header), menu_btn);
     gtk_header_bar_pack_end(GTK_HEADER_BAR(header), new_tab_btn);
+
+    g_signal_connect(menu_btn, "clicked", G_CALLBACK(tentang_terminal), NULL);
 
     notebook = gtk_notebook_new();
     gtk_notebook_set_scrollable(GTK_NOTEBOOK(notebook), TRUE);
     gtk_container_add(GTK_CONTAINER(main_window), notebook);
-
     g_signal_connect(new_tab_btn, "clicked", G_CALLBACK(on_new_tab), notebook);
     g_signal_connect(notebook, "switch-page", G_CALLBACK(on_switch_page), NULL);
-
     on_new_tab(GTK_BUTTON(new_tab_btn), notebook);
-
     gtk_widget_show_all(main_window);
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("com.vte.terminal", G_APPLICATION_NON_UNIQUE);
+    GtkApplication *app = gtk_application_new("com.indonesia.terminal", G_APPLICATION_NON_UNIQUE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
